210ba50d327056c12513d1daee4f11bbc5f6a1ed
wtetsu <tewatana@gmail.com>
2020-12-01 22:21:24 +0900

    Update packages

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -4,31 +4,35 @@ module.exports = {
   mode: process.env.NODE_ENV || "development",
   entry: {
     options: "./src/options.js",
-    content: "./src/content.js"
+    content: "./src/content.js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.jsx$/,
         use: [
           {
             loader: "babel-loader",
-            options: { presets: ["@babel/env"] }
-          }
-        ]
-      }
-    ]
+            options: { presets: ["@babel/env"] },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: [".js"]
+    extensions: [".js"],
   },
-  plugins: [new CopyWebpackPlugin([{ from: "static", to: "." }])],
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "static", to: "." }],
+    }),
+  ],
   devtool:
-    process.env.NODE_ENV === "production" ? false : "cheap-module-source-map"
+    process.env.NODE_ENV === "production" ? false : "cheap-module-source-map",
 };
